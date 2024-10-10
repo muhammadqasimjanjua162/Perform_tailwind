21ef13c75851ac80412e9006e6d73d991c86de4f
muhammadqasimashraf <135957609+muhammadqasimashraf@users.noreply.github.com>
2024-10-11 00:47:01 +0500

    working on buttons

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#243c5a",
+      },
+    },
+  },
+  plugins: [],
+};
