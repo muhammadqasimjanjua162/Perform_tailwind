e72ddd3e7ff315f8991afd132da958e69cd0ce02
muhammadqasimashraf <135957609+muhammadqasimashraf@users.noreply.github.com>
2024-10-13 01:15:28 +0500

    perfomrinTailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      fontSize: {
+        xs: "0.6rem",
+      },
       colors: {
         primary: "#243c5a",
       },
